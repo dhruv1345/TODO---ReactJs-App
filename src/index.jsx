7477f80d315ba01e34b8fd17b4c00a1a9c0bdf62
dhruv1345 <21102156@mail.jiit.ac.in>
2024-11-23 22:31:11 +0530

    started off with building todo application in reactjs

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,12 @@ import {useState} from "react";
 import '../src/App.css';
 
 export default function App(){
-    const [count, setCount] = useState(0);
-
-    function onClickHandler(){
-        setCount(count+1);
-    }
+  
 
     return(
         <div>
-            <Button onClickHandler = {onClickHandler}>
-            </Button>
+         
         </div>
     );
 }
 
-
-function Button(props){
-    return <Button onClick = {props.onClickHandler}>Counter {count}</Button>
-}
