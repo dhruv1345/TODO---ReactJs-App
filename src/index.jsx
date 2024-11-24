283492cb4590bf2e4d2dea66b1e48cd730711d53
dhruv1345 <21102156@mail.jiit.ac.in>
2024-11-24 17:50:02 +0530

    created a TODO .jsx file

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,37 @@
 import {useState} from "react";
 import '../src/App.css';
+import { toHaveDescription } from "@testing-library/jest-dom/dist/matchers";
 
 export default function App(){
   
+    const [todo, setTodo] = useState([
+        {
+            title:"go to gym",
+            discription: 'workout is necessary',
+            done:false
+        }
+    ]);
+
+    function addTodo(){
+        let newArr = [];
+        for(let i=0;i<todo.length;i++){
+            newArr.push(todo[i]);
+        }
+
+        newArr.push({
+            title:"food",
+            discription: 'necessary',
+            done:true
+        })
+
+        setTodo(newArr);
+    }
 
     return(
         <div>
-         
+            <button onClick={addTodo}>Add a todo</button>
+            {JSON.stringify(todo)}
         </div>
     );
-}
+} 
 
